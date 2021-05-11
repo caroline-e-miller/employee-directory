489c6e650c32ffe68064cf20b937467bd205961d
caroline-e-miller <caroline.miller@ymail.com>
2021-05-11 17:23:23 -0500

    added components and file basics

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./footer.css";
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <span>Employee Directory</span>
+            <span>"A phonebook of working faces."</span>
+        </footer>
+    );
+}
+
+export default Footer;
