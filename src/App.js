3f1f71c188017334d311e7f5fb18f6b20d075ee3
caroline-e-miller <caroline.miller@ymail.com>
2021-05-13 18:43:51 -0500

    simplified files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Search from "./pages/Search";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper";
+import SearchResults from "./components/SearchResults";
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <Navbar />
-        <Wrapper>
-          <Route exact path="/search" component={Search} />
-        </Wrapper>
-        <Footer />
-      </div>
-    </Router>
-  );
+  return <SearchResults />;
 }
 
 export default App;
