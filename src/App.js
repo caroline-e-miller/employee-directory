598bf49a568fcd23fc91f19b494abbd2f9fd596a
caroline-e-miller <caroline.miller@ymail.com>
2021-05-13 22:03:09 -0500

    Navbar renders

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import SearchResults from "./components/SearchResults";
+import Navbar from "./components/Navbar/Nav";
+import Wrapper from "./components/Wrapper/Wrapper"
 
 function App() {
-  return <SearchResults />;
+  return (
+    <div>
+      <Navbar />
+      <Wrapper>
+        <SearchResults />
+      </Wrapper>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -1,13 +1,11 @@
 import React from "react";
-import "./style.css";
+import "./Nav.css";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link className="navbar-brand" to="/">
-                Worldwide Workers!
-      </Link>
+            <a class="navbar-brand" href="/">Worldwide Workers!</a>
         </nav>
     );
 }
