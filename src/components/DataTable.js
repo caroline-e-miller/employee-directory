0749637f18ce4d3954f22f2afbdad36f309be7d3
caroline-e-miller <caroline.miller@ymail.com>
2021-05-16 09:35:37 -0500

    fixed DOM table nesting issue

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -3,16 +3,21 @@ import React from "react";
 function DataTable(props) {
     return (
         <table className="list-group search-results">
-            {props.results && props.results.map((item, index) => (
-                <tr key={index} className="list-group-item">
-                    <td>
-                        <img alt="Employee" src={item.picture.thumbnail} className="img-fluid" />
-                    </td>
-                    <td>
-                        {item.name.first} {item.name.last}
-                    </td>
-                </tr>
-            ))}
+            <tbody>
+                {props.results && props.results.map((item, index) => (
+                    console.log(props.results),
+                    <tr key={index} className="list-group-item">
+                        {/* <tbody> */}
+                        <td>
+                            <img alt="Employee" src={item.picture.thumbnail} className="img-fluid" />
+                        </td>
+                        <td>
+                            {item.name.first} {item.name.last} {item.email} {item.dob.date} {item.location.country}
+                        </td>
+                        {/* </tbody> */}
+                    </tr>
+                ))}
+            </tbody>
         </table>
     );
 }
